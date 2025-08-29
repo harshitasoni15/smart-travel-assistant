4fd6d4dc13012331e63fe5e25bfa2e671d90b31d
harshitasoni15 <mailtoharshitasoni@gmail.com>
2025-08-29 16:50:48 +0530

    Delete backend directory

--- backend/routes/aiRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/aiRoutes.js
-const express = require('express');
-const router = express.Router();
-const aiController = require('../controllers/aiController');
-
-// Route for planning a trip
-router.post('/plan-trip', aiController.planTrip);
-
-module.exports = router;
